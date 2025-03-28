df990185a14a1c0c684c59c1bde5bf6d972b8761
PoojanDonga2002 <poojandonga2002@gmail.com>
2025-03-28 14:53:03 +0530

    color change

--- src/Component/Form.js
+++ src/Component/Form.js
@@ -43,7 +43,7 @@ import PhoneInput from "react-phone-input-2";
 import "react-phone-input-2/lib/style.css";
 import countryList from "../countryList.json";
 import langList from "../lang.json";
-import logo1 from "../images/logo1.png";
+import logo from "../images/logo.png";
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
 import { VscAccount } from "react-icons/vsc";
@@ -689,7 +689,7 @@ const Formcomponent = () => {
           }}
         >
           <Space direction="horizontal" align="center" style={{ padding: "8px", width: "100%", justifyContent: "center" }}>
-            <img width={45} height={45} src={logo1} alt={product?.name ?? ""} />
+            <img width={45} height={45} src={logo} alt={product?.name ?? ""} />
             <Text style={{ color: "white" }}>{rData?.name ?? t("yp")}</Text>
           </Space>
           {isLicenseValid && (
